9982abcc583f740ec46aee2a27cdc193bf5411ea
= <jash.13.desai@gmail.com>
2022-08-29 03:57:42 +0530

    added 2Qs

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int y = 0;
+		while(x){
+			if(y>(INT_MAX)/10 or y<(INT_MIN)/10){
+				return 0;
+			}else{
+				y = y*10 + x%10;
+				x /= 10;
+			}
+		}
+		return y;
+    }
+};
