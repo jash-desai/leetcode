8c58b8600e2e537556a2c0b193241c6d82683c77
= <jash.13.desai@gmail.com>
2022-09-02 21:39:18 +0530

    637

--- 637. Average of Levels in Binary Tree.cpp
+++ 637. Average of Levels in Binary Tree.cpp
@@ -1,37 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
         vector<double> ans;
+        if(!root) return ans;
+        
         queue<TreeNode*> q;
         q.push(root);
-        q.push(NULL);                  
-        long long sum=0, count = 0;
+        
+        long long sum=0;
+        
         while(!q.empty()){
-            TreeNode* cur = q.front();
-            q.pop();
-            if(!cur){
-                ans.push_back((double)sum/count);     
-                sum = 0,count = 0;
-                q.push(NULL);
-                if(!q.front())break;
-            }
-            else{
-                count++;
+            sum=0;
+            int s = q.size();
+            for(int i=0; i<s; i++){
+                TreeNode* cur = q.front();
+                q.pop();
                 sum += cur->val;
-                if(cur->left)q.push(cur->left);
-                if(cur->right)q.push(cur->right);
+                if(cur->left) q.push(cur->left);
+                if(cur->right) q.push(cur->right);
             }
+            double avg = (double)sum/s;
+            ans.push_back(avg);
         }
  
         return ans;
