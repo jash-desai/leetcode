0c1961d55829e664f3caae4eec615c0db598a980
= <jash.13.desai@gmail.com>
2022-10-10 06:43:29 +0530

    1328.

---
+++ 1328. Break a Palindrome.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string breakPalindrome(string s) {
+        for(int i=0;i<s.size()/2;i++){
+            if(s[i]!='a'){
+                s[i]='a';
+                return s;
+            }
+        }
+        s[s.size()-1]='b';
+        return s.size()<2?"":s;
+    }
+};
