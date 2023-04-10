38e738f86a044bcefc93504d2f105e07b4ca9a24
= <jash.13.desai@gmail.com>
2023-04-10 05:59:06 +0530

    20.

--- 011-020/20. Valid Parentheses.cpp
+++ 011-020/20. Valid Parentheses.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(char c:s){
-            if(c=='(' or c=='[' or c=='{') st.push(c);
-            else{
-                if(st.empty()) return 0;
-                char x = st.top();
-                if((c==')' and x=='(') or (c=='}' and x=='{') or (c==']' and x=='[')) st.pop(); 
-                else return 0;
+        stack<char> stack;
+        for(char c : s){
+            switch(c){
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(c);
+                break;
+
+                case ')':
+                case '}':
+                case ']': {
+                    if(stack.empty()) return false;
+                    char compli;
+                    if(c == ')') compli = '(';
+                    if(c == '}') compli = '{';
+                    if(c == ']') compli = '[';
+                    if(stack.top() == compli){
+                        stack.pop();
+                    }else{
+                        return false;
+                    }
+                }
+                break;
+
+                default:
+                return false;
             }
+        
         }
-        return st.empty() ? 1:0;
+        return stack.empty();
     }
 };
