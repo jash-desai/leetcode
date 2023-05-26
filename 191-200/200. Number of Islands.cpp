b4b943762430442dfa20e3d3b0f5b29b74cc767b
= <jash.13.desai@gmail.com>
2023-05-27 05:15:01 +0530

    200.

--- 191-200/200. Number of Islands.cpp
+++ 191-200/200. Number of Islands.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& v,int i,int j){
-        if(i<0 || j<0 || i>=v.size() || j>=v[0].size() || v[i][j]=='0')
-            return;
-        
-        v[i][j]='0';// reset to 0 as this is already included
-        dfs(v,i-1,j);
-        dfs(v,i+1,j);
-        dfs(v,i,j-1);
-        dfs(v,i,j+1);
+    void dfs(vector<vector<char>>&v, int i,int j){
+        int n=v.size(), m=v[0].size();
+        if(i<0 or j<0 or i>=n or j>=m or v[i][j]=='0') return;
+        v[i][j]='0';
+        dfs(v,i-1,j); dfs(v,i+1,j);
+        dfs(v,i,j-1); dfs(v,i,j+1);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int res=0;
-        for(int i=0;i<grid.size();i++)
-            for(int j=0;j<grid[i].size();j++)
-                if(grid[i][j]=='1')// if 1, call the dfs and make res++ to add this island 
-                {
-                    dfs(grid,i,j);
-                    res++;
+        int ans=0;
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[i].size(); j++){
+                if(grid[i][j]=='1'){
+                    ans++; dfs(grid,i,j);
                 }
-        return res;
+            }
+        }
+        return ans;
     }
 };
