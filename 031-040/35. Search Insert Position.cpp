65b50a12d83d567679ff459b90ead602f3088ea6
= <jash.13.desai@gmail.com>
2023-06-01 08:43:27 +0530

    35.

--- 031-040/35. Search Insert Position.cpp
+++ 031-040/35. Search Insert Position.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        int low = 0, high = nums.size()-1;
-        while (low <= high) {
-            int mid = low + (high-low)/2;
-
-            if (nums[mid] < target)
-                low = mid+1;
-            else
-                high = mid-1;
+    int searchInsert(vector<int>&v, int k) {
+        int l=0, r=v.size()-1; int m;
+        while(l<=r){
+            m = (l+r)/2;
+            if(v[m]==k) return m;
+            if(v[m]<k) l=m+1;
+            else r=m-1;
         }
-        return low;
+        return l;
     }
 };
