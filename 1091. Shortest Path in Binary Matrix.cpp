bf74c3c0cda8e1790572928f60c9dff47cc2a517
= <jash.13.desai@gmail.com>
2023-06-01 08:44:25 +0530

    1091.

--- 1091. Shortest Path in Binary Matrix.cpp
+++ 1091. Shortest Path in Binary Matrix.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>&v) {
-        int n=v.size(); vector<vector<bool>> vis(n, vector<bool>(n,0));
-        if(v[0][0]==1 or v[n-1][n-1]==1) return -1;
-        queue<pair<pair<int,int>,int>> q;
-        q.push({{0,0},1}); vis[0][0]=1;
+        int n = v.size();
+        if(v[0][0] or v[n-1][n-1]) return -1;
+        queue<pair<int,pair<int,int>>> q; q.push({1,{0,0}});
+        vector<vector<bool>> vis(n, vector<bool>(n,0)); vis[0][0]=1;
         while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int d = q.front().second; q.pop();
+            int r = q.front().second.first;
+            int c = q.front().second.second;
+            int d = q.front().first; q.pop();
             if(r==n-1 and c==n-1) return d;
             for(int i=-1; i<2; i++){
                 for(int j=-1; j<2; j++){
                     int nr = r+i; int nc = c+j;
                     if(nr>=0 and nr<n and nc>=0 and nc<n){
-                        if(v[nr][nc]==0 and !vis[nr][nc]){
-                            q.push({{nr,nc},d+1});
+                        if(!vis[nr][nc] and v[nr][nc]==0){
+                            q.push({d+1,{nr,nc}});
                             vis[nr][nc]=1;
                         }
                     }
