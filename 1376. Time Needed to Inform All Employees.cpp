7878cba73bca57e88010cf55f9338b2317a4164c
= <jash.13.desai@gmail.com>
2023-06-03 09:38:36 +0530

    changes.

--- 1376. Time Needed to Inform All Employees.cpp
+++ 1376. Time Needed to Inform All Employees.cpp
@@ -5,13 +5,11 @@ public:
         for(int i=0; i<n; i++){
             if(v[i]!=-1) adj[v[i]].push_back(i);
         }
-        vector<int> dis(n,0); dis[h]=0;
-        queue<int> q; q.push(h);
+        vector<int> dis(n,0); dis[h]=0; queue<int> q; q.push(h);
         while(!q.empty()){
             int u = q.front(); q.pop();
             for(int v : adj[u]){
-                dis[v]=dis[u]+t[u]; a = max(a,dis[v]);
-                q.push(v);
+                dis[v]=dis[u]+t[u]; a = max(a,dis[v]); q.push(v);
             }
         }
         return a;
