7866ad5a1aef1046e71a3f3d9be045dee422a65c
= <jash.13.desai@gmail.com>
2023-06-03 09:36:24 +0530

    1376.

---
+++ 1376. Time Needed to Inform All Employees.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numOfMinutes(int n, int h, vector<int>&v, vector<int>&t){
+        vector<vector<int>> adj(n); int a = t[h];
+        for(int i=0; i<n; i++){
+            if(v[i]!=-1) adj[v[i]].push_back(i);
+        }
+        vector<int> dis(n,0); dis[h]=0;
+        queue<int> q; q.push(h);
+        while(!q.empty()){
+            int u = q.front(); q.pop();
+            for(int v : adj[u]){
+                dis[v]=dis[u]+t[u]; a = max(a,dis[v]);
+                q.push(v);
+            }
+        }
+        return a;
+    }
+};
