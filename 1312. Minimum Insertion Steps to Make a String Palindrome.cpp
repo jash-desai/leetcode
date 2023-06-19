27f7e504a355983338b83c8ee58c057450ef3986
= <jash.13.desai@gmail.com>
2023-06-19 13:12:58 +0530

    1312.

--- 1312. Minimum Insertion Steps to Make a String Palindrome.cpp
+++ 1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -1,30 +1,22 @@
 class Solution {
-public:
-    int lcs(string t, string s, vector<vector<int>>&dp){
-        int n = s.size();
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= n; j++){
-                if(i == 0 || j == 0)
-                dp[i][j] = 0;
+private:
+    int lcs(string&a, string&b, int n, int m){
+        if(n==0 or m==0) return 0;
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<int> curr(m+1,0), prev(m+1,0);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(a[i-1]==b[j-1]) curr[j] = 1 + prev[j-1];
+                else curr[j] = max(prev[j], curr[j-1]);
             }
+            prev=curr;
         }
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                if(s[i-1] == t[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-        return dp[n][n];
+        return prev[m];
     }
-    
-    int minInsertions(string s) {
-        string t = s;
-        reverse(t.begin(),t.end());
-        int n = s.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        return n - lcs(s,t,dp);
+public:
+    int minInsertions(string a) {
+        int n = a.size(); string b(a.rbegin(), a.rend());
+        // reverse(b.begin(), b.end());
+        return n - lcs(a,b,n,n);
     }
 };
