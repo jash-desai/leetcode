7a89e547965a27398e50c771ff2b8b5a350be88c
= <jash.13.desai@gmail.com>
2023-06-19 10:52:49 +0530

    changes.

--- 511-520/516. Longest Palindromic Subsequence.cpp
+++ 511-520/516. Longest Palindromic Subsequence.cpp
@@ -1,17 +1,22 @@
 class Solution {
-public:
-    int longestPalindromeSubseq(string s) {
-        int n = s.length();
-        vector<int> dp(n, 0);
-        for(int i=n-1; i>=0; i--){
-            vector<int> newdp(n, 0);
-            newdp[i] = 1;
-            for(int j=i+1; j<n; j++){
-                if(s[i] == s[j]) newdp[j] = 2 + dp[j-1];
-                else newdp[j] = max(dp[j], newdp[j-1]);
+private:
+    int lcs(string&a, string&b, int n, int m){
+        if(n==0 or m==0) return 0;
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<int> curr(m+1,0), prev(m+1,0);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(a[i-1]==b[j-1]) curr[j] = 1 + prev[j-1];
+                else curr[j] = max(prev[j], curr[j-1]);
             }
-            dp = newdp;
+            prev=curr;
         }
-        return dp[n-1];
+        return prev[m];
+    }
+public:
+    int longestPalindromeSubseq(string a) {
+        int n = a.size(); string b = a;
+        reverse(b.begin(), b.end());
+        return lcs(a,b,n,n);
     }
 };
