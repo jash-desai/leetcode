3948319684f4bf9c9cf1605fc4cf24bfcea8d39f
= <jash.13.desai@gmail.com>
2023-06-19 14:15:30 +0530

    583.

---
+++ 583. Delete Operation for Two Strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int lcs(string&a, string&b, int n, int m){
+        if(n==0 or m==0) return 0;
+        vector<int> curr(m+1, 0), prev(m+1, 0);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(a[i-1]==b[j-1]) curr[j] = 1 + prev[j-1];
+                else curr[j] = max(prev[j], curr[j-1]);
+            }
+            prev=curr;
+        }
+        return prev[m];
+    }
+public:
+    int minDistance(string a, string b) {
+        int n = a.size(), m = b.size();
+        if(n==0) return m;
+        if(m==0) return n;
+        return (n-lcs(a,b,n,m)) + (m-lcs(a,b,n,m));
+    }
+};
