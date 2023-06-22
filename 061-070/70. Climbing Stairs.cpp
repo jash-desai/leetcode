a068943a20a59fbb8e6855c02df72109e6181af7
= <jash.13.desai@gmail.com>
2023-06-23 05:29:32 +0530

    changes.

--- 061-070/70. Climbing Stairs.cpp
+++ 061-070/70. Climbing Stairs.cpp
@@ -1,12 +1,30 @@
 class Solution {
+private:
+    int rec(int n, vector<int>&dp){
+        if(n==1 or n==2 or n==3) return n;
+        if(dp[n] != 0) return dp[n]; 
+        int ans1 = rec(n-1,dp);
+        int ans2 = rec(n-2,dp);
+        return dp[n] = (ans1+ans2);
+    }
 public:
     int climbStairs(int n) {
-        vector<int> steps(n,0);
-        if(n==1 or n==2) return n;
-        steps[0]=1; steps[1]=2;
-        for(int i=2; i<n; i++){
-            steps[i]=steps[i-2]+steps[i-1];
+        if(n==1 or n==2 or n==3) return n;
+        // vector<int> dp(n+1, 0);
+        // return rec(n,dp);
+        // for(int i=0; i<4; i++) dp[i]=i;
+        // for(int i=4; i<=n; i++){
+        //     int ans1 = dp[i-1];
+        //     int ans2 = dp[i-2];
+        //     dp[i] = ans1+ans2;
+        // }
+        // return dp[n];
+        int curr=0, prev1=3, prev2=2;
+        for(int i=4; i<=n; i++){
+            curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return steps[n-1];
+        return prev1;
     }
 };
