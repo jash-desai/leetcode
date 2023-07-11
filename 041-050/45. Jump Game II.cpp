3347f2f184513ba0f5d7abdd91248193eeb43af7
= <jash.13.desai@gmail.com>
2023-07-11 14:13:07 +0530

    changes.

--- 041-050/45. Jump Game II.cpp
+++ 041-050/45. Jump Game II.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int jump(vector<int> &v) {
-        int n=v.size();
-        if(n<2) return 0;
-        int cur_end = 0, cur_farthest = 0, jumps = 0;
-        for (int i=0; i<n-1; i++) {
-            cur_farthest = max(cur_farthest, i + v[i]);
-            if (i == cur_end) {
-              jumps++;
-              cur_end = cur_farthest;
+    int jump(vector<int>&v) {
+        if(size(v) < 2) return 0;
+        int n = size(v), ans=0, curr=0, maxCurr=0;
+        for(int i=0; i<n-1; i++){
+            curr = max(curr, v[i]+i);
+            if(i==maxCurr){
+                ans++;
+                maxCurr = curr;
+                if(maxCurr == n-1) return ans;
             }
         }
-        return jumps;
+        return ans;
     }
 };
--- 231-240/237. Delete Node in a Linked List.cpp
+++ 231-240/237. Delete Node in a Linked List.cpp
@@ -1,17 +1,8 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode *nextNode = node->next;
-        node->val = nextNode->val;
-        node->next = nextNode->next;
-        delete nextNode;
+        node->val = node->next->val;
+        node->next = node->next->next;
+        return;
     }
 };
