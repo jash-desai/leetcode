7ca344cfa43b5bb00b7ae2eeedd8f9147eee1ec0
= <jash.13.desai@gmail.com>
2023-07-14 17:26:07 +0530

    changes.

--- 001-010/4. Median of Two Sorted Arrays.cpp
+++ 001-010/4. Median of Two Sorted Arrays.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
-        int na=a.size();    int nb=b.size();
-        int n=na+nb;
-        vector<int> v(n);
-        int i=0, j=0;
-        int k=0;
-        while(i<na and j<nb){
-            if(a[i]<b[j]){
-                v[k++] = a[i++];
-            }else{
-                v[k++] = b[j++];
+    double findMedianSortedArrays(vector<int>&a, vector<int>&b) {
+        if(a.empty()){
+            int idx = size(b)/2;
+            if(size(b)%2) return (double)b[idx];
+            return (b[idx] + b[idx - 1]) / 2.0;
+        }else if(b.empty()){
+            int idx = size(a)/2;
+            if(size(a)%2) return (double)a[idx];
+            return (a[idx] + a[idx - 1]) / 2.0;
+        }
+        if(size(a)>size(b)) return findMedianSortedArrays(b,a);
+        int na = size(a), nb = size(b), n = na+nb;
+        int l = 0, r = na;
+        while(l<=r){
+            int i = l + ((r-l) >> 1);
+            int j = ((n+1) >> 1) - i;
+            int l1 = (i==0) ? INT_MIN : a[i-1];
+            int l2 = (j==0) ? INT_MIN : b[j-1];
+            int r1 = (i==na) ? INT_MAX : a[i];
+            int r2 = (j==nb) ? INT_MAX : b[j];
+            if(l1<=r2 and l2<=r1){
+                if(n%2) return (double)max(l1, l2);
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
             }
+            if(l1 > r2) r = i-1;
+            else l = i+1;
         }
-        while(i<na){
-            v[k++] = a[i++];
-        }
-        while(j<nb){
-            v[k++] = b[j++];
-        }
-        
-        if(n%2==1){
-            return (double)v[(n)/2];
-        }else{
-            double x = v[n/2 - 1] + v[n/2];
-            return (double)x/2;
-        }
+        return 0.0;
     }
 };
