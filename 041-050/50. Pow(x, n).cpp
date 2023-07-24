f31f54e05284a49a7677ee10c0ce3802db703b27
= <jash.13.desai@gmail.com>
2023-07-24 08:18:31 +0530

    50.

--- 041-050/50. Pow(x, n).cpp
+++ 041-050/50. Pow(x, n).cpp
@@ -3,9 +3,8 @@ public:
     double myPow(double x, int n) {
         if(n==0) return 1;
         // using logarithms
-        return (x<0) ? (n%2==0 ? (exp(n*log(-x))) : -exp(n*log(-x))) : exp(n*log(x));
+        return (x<0) ? (!(n%2) ? (exp(n*log(-x))) : -exp(n*log(-x))) : exp(n*log(x));
         // using in-built pow fxn
         return pow(x,n);
-
     }
 };
