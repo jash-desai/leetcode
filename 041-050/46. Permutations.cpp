90ff4e35374dfb7bcfb561e64bdf9498d95a8b28
= <jash.13.desai@gmail.com>
2023-08-02 06:17:28 +0530

    46.

--- 041-050/46. Permutations.cpp
+++ 041-050/46. Permutations.cpp
@@ -1,21 +1,20 @@
 class Solution {
-public:
-    
+private:
     void rec(vector<int>&v, vector<vector<int>>&a, int idx){
         if(idx==v.size()){
-            a.push_back(v);
-            return;
+            a.push_back(v); return;
+        }else{
+            for(int i=idx; i<v.size(); i++){
+                swap(v[i], v[idx]);
+                rec(v,a,idx+1);
+                swap(v[i], v[idx]);
+            }
         }
-        for(int i=idx; i<v.size(); i++){
-            swap(v[i], v[idx]);
-            rec(v,a,idx+1);
-            swap(v[i], v[idx]);
-        }
+        return;
     }
-    
+public:    
     vector<vector<int>> permute(vector<int>&v) {
         vector<vector<int>> a;
-        rec(v,a,0);
-        return a;
+        rec(v,a,0); return a;
     }
 };
