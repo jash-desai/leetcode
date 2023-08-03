9ee49ed97a7ebea342b5eba36f066acc3df82b7a
= <jash.13.desai@gmail.com>
2023-08-03 05:55:35 +0530

    17.

--- 011-020/17. Letter Combinations of a Phone Number.cpp
+++ 011-020/17. Letter Combinations of a Phone Number.cpp
@@ -1,25 +1,20 @@
 class Solution {
+    vector<string> m = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 public:
-    void rec(string s, vector<string>&ans, string temp, string m[], int idx){
-        if(idx>=s.size()){
-            ans.push_back(temp);
-            return;
-        }
-        int n=s[idx]-'0';
-        string val=m[n];
-        for(int i=0; i<val.size(); i++){
-            temp.push_back(val[i]);
-            rec(s,ans,temp,m,idx+1);
+    void rec(string&s, vector<string>&ans, string temp, int idx){
+        if(idx>=s.size()){ ans.push_back(temp); return; }
+        int n = s[idx]-'0';
+        for(char c : m[n]){
+            temp.push_back(c);
+            rec(s,ans,temp,idx+1);
             temp.pop_back();
         }
     }
     
     vector<string> letterCombinations(string s) {
-        vector<string> ans;
-        if(!s.size()) return ans;
-        string temp;
-        string m[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        rec(s,ans,temp,m,0);
+        if(!s.size()) return {};
+        vector<string> ans; string temp;
+        rec(s,ans,temp,0);
         return ans;
     }   
 };
