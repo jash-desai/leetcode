8755ffa21453df49f2102d5beaf1482cdba4e67e
= <jash.13.desai@gmail.com>
2023-08-07 06:22:09 +0530

    74.

--- 071-080/74. Search a 2D Matrix.cpp
+++ 071-080/74. Search a 2D Matrix.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& v, int k) {
-        int n = size(v), m = size(v[0]);
-        /* two pointers approach - 
-        int i = 0, j = m-1;
+        /*
+        // Two Pointer Approach :
+        int n = v.size(), m = v[0].size(), i{0}, j{m-1};
         while(i<n and j>=0){
-            if(v[i][j]==k) return 1;
-            else if(v[i][j] < k) i++;
-            else j--;
-        } return 0;
+            if(v[i][j] == k) return true;
+            else if(v[i][j] > k) j--;
+            else i++;
+        } return false;
         */
-        /* binary search approach - */
-        int l = 0, r = (n*m)-1, mid;
+
+        // Binary search Approach :
+        int n = v.size(), m = v[0].size(), l{0}, r={(n*m)-1};
         while(l<=r){
-            mid = l + ((r-l) >>1);
-            int i = mid/m, j = mid%m;
-            if(v[i][j] == k) return 1;
-            else if(v[i][j] < k) l = mid+1;
+            int mid = l + ((r-l) >> 1);
+            int row = mid/m, col = mid%m;
+            if(v[row][col] == k) return true;
+            else if(v[row][col] < k) l = mid+1;
             else r = mid-1;
-        } return 0;
+        } return false;
     }
 };
