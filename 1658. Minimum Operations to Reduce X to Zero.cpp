801563fc7e123f88ef0e8f6569140ac65acd6942
jash-desai <jash.13.desai@gmail.com>
2023-09-20 08:55:36 +0530

    .

--- 1658. Minimum Operations to Reduce X to Zero.cpp
+++ 1658. Minimum Operations to Reduce X to Zero.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int minOperations(vector<int>&v, int x) {
-        int n=v.size();
-        int ans=0;
-        int s=0, e=0;
+        int n=v.size(), ans=0, s=0, e=0;
         int ts=accumulate(v.begin(), v.end(),0);
         if(ts==x) return n;
-        int sum=ts-x;
-        int cs=0;
+        int sum=ts-x, cs=0;
         while(e<n){
             cs+=v[e];
             while(s<e && cs>sum){
