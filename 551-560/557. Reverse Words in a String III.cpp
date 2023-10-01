5cd6b5dd52b3169fc63dbe011b3607385c616cf8
jash-desai <jash.13.desai@gmail.com>
2023-10-01 08:02:35 +0530

    557.

--- 551-560/557. Reverse Words in a String III.cpp
+++ 551-560/557. Reverse Words in a String III.cpp
@@ -1,28 +1,44 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         vector<string> a; string t;
+//         for(int i=0; i<s.size(); i++){
+//             if(s[i]==' '){
+//                 a.push_back(t);
+//                 t.clear();
+//             }else if(i==s.size()-1){
+//                 t += s[i];
+//                 a.push_back(t);
+//                 t.clear();
+//             }else{
+//                 t += s[i];
+//             }
+//         }
+//         for(int i=0; i<a.size(); i++){
+//             reverse(begin(a[i]), end(a[i]));
+//         }
+//         t.clear();
+//         for(int i=0; i<a.size(); i++){
+//             t += a[i];
+//             t += " ";
+//         }
+//         t.pop_back();
+//         return t;
+//     }
+// };
+
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> a; string t;
-        for(int i=0; i<s.size(); i++){
-            if(s[i]==' '){
-                a.push_back(t);
-                t.clear();
-            }else if(i==s.size()-1){
-                t += s[i];
-                a.push_back(t);
-                t.clear();
-            }else{
-                t += s[i];
+        int n = s.size(); stack<char> st; string ans;
+        for(char &c : s){
+            if(c != ' ') st.push(c);
+            else{
+                while(st.size()) ans += st.top(), st.pop();
+                ans += " ";
             }
         }
-        for(int i=0; i<a.size(); i++){
-            reverse(begin(a[i]), end(a[i]));
-        }
-        t.clear();
-        for(int i=0; i<a.size(); i++){
-            t += a[i];
-            t += " ";
-        }
-        t.pop_back();
-        return t;
+        while(st.size()) ans += st.top(), st.pop();
+        return ans;
     }
 };
