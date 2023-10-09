efc931086d474938d584458bf6bafa6a52f54f6c
jash-desai <jash.13.desai@gmail.com>
2023-10-09 06:53:14 +0530

    34.

--- 031-040/34. Find First and Last Position of Element in Sorted Array.cpp
+++ 031-040/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -1,41 +1,28 @@
 class Solution {
 private:
-    int fp(vector<int>&v, int k){
-        int n=v.size();
-        int l=0, r=n-1, m;
+    int first(vector<int>&v, int k){
+        int n = v.size(), l = 0, r = n-1, ans = n;
         while(l<=r){
-            m = (l+r)>>1;
-            if(v[m]==k and (m==0 or (m!=0 and v[m-1]!=k))) return m;
-            if(v[m]==k and v[m-1]==k){
-                r=m-1;
-            }
-            if(v[m]>k) r=m-1;
-            else if(v[m]<k) l=m+1;
+            int m = (l + ((r-l) >> 1));
+            if(v[m]==k) ans = m, r = m-1;
+            else if(v[m]<k) l = m+1;
+            else r = m-1;
         }
-        return -1;
+        return (ans==n) ? -1 : ans;
     }
-    int lp(vector<int>&v, int k){
-        int n=v.size();
-        int l=0, r=n-1, m;
+    int last(vector<int>&v, int k){
+        int n = v.size(), l = 0, r = n-1, ans = -1;
         while(l<=r){
-            m = (l+r)>>1;
-            if(v[m]==k and (m==n-1 or (m!=n-1 and v[m+1]!=k))) return m;
-            if(v[m]==k and v[m+1]==k){
-                l=m+1;
-            }
-            if(v[m]>k) r=m-1;
-            else if(v[m]<k) l=m+1;
+            int m = (l + ((r-l) >> 1));
+            if(v[m]==k) ans = m, l = m+1;
+            else if(v[m]<k) l = m+1;
+            else r = m-1;
         }
-        return -1;
+        return (ans==-1) ? -1 : ans;
     }
 public:
     vector<int> searchRange(vector<int>&v, int k) {
-        vector<int> a(2,-1);
-        if(v.empty()) return a;
-        int left = fp(v, k);
-        if(left == -1) return a;
-        int right = lp(v, k);
-        a[0] = left; a[1] = right;
-        return a;
+        int n = v.size(), fp = first(v,k), lp = last(v,k);
+        return {fp, lp};
     }
 };
