75280435038f53e99b4e4d089d06b9c2cdeccda6
jash-desai <jash.13.desai@gmail.com>
2023-10-23 08:52:36 +0530

    342.

--- 341-350/342. Power of Four.cpp
+++ 341-350/342. Power of Four.cpp
@@ -2,12 +2,21 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n<1) return 0;
-        int t=n;
-        int count=0;
-        while(t>1){
-            t >>= 2;
-            count+=2;
+        // int t=n;
+        // int count=0;
+        // while(t>1){
+        //     t >>= 2;
+        //     count+=2;
+        // }
+        // return (t<<count)==n ? 1 : 0;
+        if (n > 0 && (n & (n - 1)) == 0) {
+            int zero_count = 0;
+            while (n > 1) {
+                zero_count++;
+                n >>= 1;
+            }
+            return zero_count % 2 == 0;
         }
-        return (t<<count)==n ? 1 : 0;
+        return false;
     }
 };
