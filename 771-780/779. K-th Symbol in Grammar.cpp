7406e0e08cf974907938d9657e94f0407c098241
jash-desai <jash.13.desai@gmail.com>
2023-10-25 07:22:15 +0530

    779.

--- 771-780/779. K-th Symbol in Grammar.cpp
+++ 771-780/779. K-th Symbol in Grammar.cpp
@@ -1,15 +1,23 @@
 class Solution {
+    int powTwo[31];
+private:
+    void power(){
+        powTwo[0] = 1;
+        for(int i=1; i<31; i++){
+            powTwo[i] = powTwo[i-1]*2;
+        }
+        return;
+    }
+    int rec(int n, int k){
+        if(n==1 or k==1) return 0;
+        int l = powTwo[n-1];
+        int m = l/2;
+        if(k<=m) return rec(n-1,k);
+        return (1 - rec(n-1,k-m));
+    }
 public:
     int kthGrammar(int n, int k) {
-        if(n==1 and k==1){
-            return 0;
-        }
-        int l = pow(2,n-1);
-        int m = l/2;
-        if(k<=m){
-            return kthGrammar(n-1, k);
-        }else{
-            return 1- kthGrammar(n-1, k-m);
-        }
+        power();
+        return rec(n,k);
     }
 };
